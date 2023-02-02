392bcc8ae1748a51015c3645a0dfbce670396df2
masachika <masachika.kmd@gmail.com>
2023-02-02 14:00:44 +0900

    実行スクリプトの追加

---
+++ run.ps1
@@ -0,0 +1,6 @@
+python main_test_swinir.py `
+    --task real_sr `
+    --model_path 003_realSR_BSRGAN_DFOWMFC_s64w8_SwinIR-L_x4_GAN.pth `
+    --folder_lq testsets/custom `  # path to low-quality images
+    --scale 4 `
+    --large_model
